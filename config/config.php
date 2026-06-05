72f223e8772455f5662d7e5ee5c8968fe3286455
Mizanur Rahman <mizanurrahmanedge@outlook.com>
2026-06-06 00:20:37 +0600

    Update config.php with user credentials

--- config/config.php
+++ config/config.php
@@ -1,13 +1,12 @@
 <?php
 // Base URL of the application
-define('SITE_URL', 'http://localhost/Newspaper website'); // Adjust this for production
+define('SITE_URL', 'http://extract.mailszo.com'); // Adjust this for production
 
 // Database Credentials
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', ''); // Change as needed
-define('DB_NAME', 'newspaper_db');
-
+define('DB_USER', 'newspaper');
+define('DB_PASS', 'newspaper'); // Change as needed
+define('DB_NAME', 'newspaper');
 // Site Information
 define('SITE_NAME', 'মডার্ন নিউজ');
 define('SITE_DESC', 'সর্বশেষ খবরের জন্য আপনার বিশ্বস্ত মাধ্যম।');
